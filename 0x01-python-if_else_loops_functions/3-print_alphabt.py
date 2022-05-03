3079ac06475b5ad07ba05a6cb214de1a4fbab702
Carlos Berrio <4634@holbertonschool.com>
2022-05-03 13:42:06 -0500

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i != 101 and i != 113):
-        print("{:c}".format(i), end='')
-        
+for k in range(97, 123):
+    if (k != 101 and k != 113):
+        print(chr(k), end='')
