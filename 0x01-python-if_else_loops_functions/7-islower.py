f630c6a24aa9d9ee9e7b7aefff3c3f55b9daa161
Carlos Berrio <4634@holbertonschool.com>
2022-05-03 16:11:44 -0500

    task8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) in range(97, 123):
-        return True
-    else:
-        return False
+def uppercase(str):
+    for i in str:
+        pt = ord(i)
+        if pt >= 97 and pt <= 122:
+            pt -= 32
+        print('{:c}'.format(pt), end="")
+    print()
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    if ord(str) in range(97, 123):
+        print(str)
