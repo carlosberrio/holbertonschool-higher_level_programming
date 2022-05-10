eabe192a0b7fefbfef7bffebb45746a8a06d49cb
Carlos Berrio <4634@holbertonschool.com>
2022-05-09 21:55:58 -0500

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        mayor = my_list[0]
+        for number in my_list:
+            if number > mayor:
+                mayor = number
+        return mayor
