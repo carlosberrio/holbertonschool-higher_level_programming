d5552902ca177510c922a82826763d01c38b0c62
Carlos Berrio <4634@holbertonschool.com>
2022-05-09 22:26:27 -0500

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def main():
-    for i in dir(hidden_4):
-        if(i[0] != "_" and i[1] != "_"):
-            print(i)
-
-
 if __name__ == "__main__":
-    main()
+    import hidden_4
+    for name in dir(hidden_4):
+        if "__" not in name[:2]:
+            print(name)
