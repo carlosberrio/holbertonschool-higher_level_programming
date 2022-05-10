f6bf7292baf1d53e23006a0f4259c51c3c08a4d9
Carlos Berrio <4634@holbertonschool.com>
2022-05-10 16:44:06 -0500

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    big = max(a_dictionary.items())
-    return big[0]
+    if a_dictionary:
+        return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
