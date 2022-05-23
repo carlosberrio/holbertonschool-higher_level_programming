3688a797073fa361f38480b1f2830b86a587cac0
Carlos Berrio <4634@holbertonschool.com>
2022-05-23 12:02:39 -0500

    commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from ast import Pass
+
+
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for idx in range(x):
+        try:
+            print(my_list(idx), end="")
+            c += 1
+        except:
+            pass
+    print("")
+    return c
