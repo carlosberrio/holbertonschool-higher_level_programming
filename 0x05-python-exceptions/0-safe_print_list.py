3a8ddfc1993f2d79cd73e06d05bd1ec62ec0fa93
Carlos Berrio <4634@holbertonschool.com>
2022-05-23 12:19:01 -0500

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list(idx), end="")
             c += 1
-        except IndexError:
+        except:
             pass
     print("")
     return c
