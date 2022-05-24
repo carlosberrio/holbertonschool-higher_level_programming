c5bceab51a553317efae0fd4584e5218c2e2a323
Carlos Berrio <4634@holbertonschool.com>
2022-05-24 15:04:42 -0500

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): length of a side of the square
+        """
+        self.__size = size
