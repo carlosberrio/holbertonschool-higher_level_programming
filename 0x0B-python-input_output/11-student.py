4c1c6086a0f06f266632dc3a7e7c6ca726b559dc
Carlos Berrio <4634@holbertonschool.com>
2022-06-09 17:33:08 -0500

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module with the class Student"""
+
+
+class Student:
+    """A simple class with one method"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method that retrieves a dictionary
+        representation of a Student instance
+        """
+        n_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for attributes in attrs:
+                if attributes in self.__dict__:
+                    n_dict.update({attributes: self.__dict__[attributes]})
+            return n_dict
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """method that replaces all attributes of the Student instance"""
+        for data in json:
+            self.__dict__.update({data: json[data]})
