ac0ec229a9aa2832e6318b5ad72866c36b8cd62f
Carlos Berrio <4634@holbertonschool.com>
2022-06-09 17:12:08 -0500

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the function class_to_json"""
+
+
+def class_to_json(obj):
+    """return the dictionary desctipyion
+    with simple data structure"""
+    return obj.__dict__
