25cbbb172f441eef998070e6f13835f9e6ef10bb
Carlos Berrio <4634@holbertonschool.com>
2022-06-09 16:32:53 -0500

    task6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Module for from_json_string method"""
+"""
+contains load
+"""
 import json
 
 
-def from_json_string(my_str):
-    """Returns an object (Python data structure) <my_str>
-    represented by a JSON string"""
-    return json.load(my_str)
+def load_from_json_file(filename):
+    """creates an object
+    from a json"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
