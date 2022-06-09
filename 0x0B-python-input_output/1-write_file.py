8b7f1b2935f865780a149ad58924b88807c37a86
Carlos Berrio <4634@holbertonschool.com>
2022-06-08 19:45:09 -0500

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,9 +5,8 @@ contains writes a string to a text file (UTF8)
 
 
 def write_file(filename="", text=""):
-    """unction that writes a string to a text file (UTF8) and returns the 
+    """unction that writes a string to a text file (UTF8) and returns the
     number of characters written
     """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
-        #return (len(text))
