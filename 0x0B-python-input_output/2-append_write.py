c22330ce584724a628d90580f4eba4b1576d06b7
Carlos Berrio <4634@holbertonschool.com>
2022-06-08 19:46:30 -0500

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Module for read_lines method"""
+"""
+contains appends a string at the end of a text file (UTF8)
+"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads n lines of a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as file:
-        if nb_lines <= 0:
-            print(file.read(), end="")
-            return
-        for n, line in enumerate(file):
-            if n == nb_lines:
-                break
-            print(line, end="")
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file (UTF8) and
+    returns the number of characters added"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
