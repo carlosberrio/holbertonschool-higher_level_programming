b324c8ee13a694d0fb606f2e6dcccbcc15ceb1b0
Carlos Berrio <4634@holbertonschool.com>
2022-06-13 15:39:46 -0500

    commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,14 +7,17 @@
 from curses.textpad import rectangle
 from models.rectangle import Rectangle
 
-class Square(Rectangle)
-"""
-...
-"""
+class Square(Rectangle):
+    """
+    ...
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id)
-        
-        self.__size = size
-        self.x = x
-        self.y = y
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        ...
+        """
+        return str(f"[Square] ({self.id}) <self.x>/<self.y> - <self.size>)")
