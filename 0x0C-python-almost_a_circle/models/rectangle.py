fdd954666f76462bd1c0fd1f9d480eefc66426d9
Carlos Berrio <4634@holbertonschool.com>
2022-06-13 16:55:45 -0500

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -139,4 +139,12 @@ class Rectangle(Base):
                     self.__x = value
                 if k == "y":
                     self.__y = value
-                    
+
+    def to_dictionary(self):
+        """
+        ...
+        """
+        my_dictionary = {"id": self.id, "width": self.__width,
+                         "height": self.__height, "x": self.__x, "y": self.__y}
+        return my_dictionary
+
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,4 +20,50 @@ class Square(Rectangle):
         """
         ...
         """
-        return str(f"[Square] ({self.id}) <self.x>/<self.y> - <self.size>)")
+        return str(f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
+
+    @property
+    def size(self):
+        """the width of the Rectangle."""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+        
+    def update(self, *args, **kwargs):
+        """
+        ...
+        """ 
+        if args is not None and len(args) > 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for k, value in kwargs.items():
+                if k == "id":
+                    self.id = value
+                if k == "size":
+                    self.size = value
+                if k == "x":
+                    self.x = value
+                if k == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """
+        ...
+        """
+        my_dictionary = {"id": self.id, "size": self.size,
+                         "x": self.x, "y": self.y}
+        return my_dictionary
