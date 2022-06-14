9ab99060df015c6d1e460859952d066b762e3a1e
Carlos Berrio <4634@holbertonschool.com>
2022-06-14 12:19:15 -0500

    commit

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains tests for Base class
+"""
+
+import os
+import unittest
+import pep8
+import inspect
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from models import base
+
+
