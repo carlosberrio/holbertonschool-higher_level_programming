84c7dc2e7f9a8491e7d28e1bcfe50db611f1dce2
Carlos Berrio <4634@holbertonschool.com>
2022-07-13 13:18:21 -0500

    task5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Script that prints the full description of the table 'first_table' from a database in a MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+SHOW CREATE TABLE first_table;
