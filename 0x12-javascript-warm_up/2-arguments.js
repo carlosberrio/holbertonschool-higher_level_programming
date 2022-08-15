3dbb40776246bbf8de3ca7e719fcc175afdf2ccd
Carlos Berrio <4634@holbertonschool.com>
2022-08-15 12:29:10 -0500

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-process.argv.length === 2 ? console.log('No argument')
-  : process.argv.length === 3 ? console.log('Argument found')
-    : console.log('Arguments found');
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
