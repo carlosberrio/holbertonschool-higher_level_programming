4079d63330860015c1092009cdccee259da311c7
Carlos Berrio <4634@holbertonschool.com>
2022-08-16 18:32:25 -0500

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const Square1 = require('./5-square');
+const square = require('./5-square');
 
-class Square extends Square1 {
-  charPrint (c = 'X') {
-    for (let s = 0; s < this.width; s++) {
-      console.log(c.repeat(this.width));
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 }
-
 module.exports = Square;
