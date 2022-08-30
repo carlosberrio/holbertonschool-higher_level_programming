a15543e80708f42178625ec3706bf457566cc0fc
carlosberrio <carlosberriomon@yahoo.com>
2022-08-30 16:18:49 -0500

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """script that lists all states from the database hbtn_0e_0_usa:
 using module MySQLdb (import MySQLdb):
     Connects to a MySQL server running on localhost at port 3306
-    Port 3306 is the default port for the MySQL Protocol ( port ), which is used
+    Port 3306 is the default port for the MySQL Protocol ( port ), which isused
     by the mysql client, MySQL Connectors, and utilities such as
     mysqldump and mysqlpump."""
 
@@ -12,9 +12,10 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cur = db.cursor()
-            cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-                rows = cur.fetchall()
-                    for row in rows:
-                                print(row)
-
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
