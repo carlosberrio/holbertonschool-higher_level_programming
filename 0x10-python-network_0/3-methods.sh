b60744df607289aed33246ba0e5907e47b5941ca
Carlos Berrio <4634@holbertonschool.com>
2022-09-02 09:50:29 -0500

    task3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep Allow | cut --delimiter=":" -f2
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
